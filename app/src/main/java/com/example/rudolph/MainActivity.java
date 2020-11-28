21cbb02c9751d6c589ecff1cc541bc75e9f2a35d
Megan Ren <megan.j.ren.23@dartmouth.edu>
2020-11-27 22:39:04 -0500

    layout of gift search page

--- app/src/main/java/com/example/rudolph/MainActivity.java
+++ app/src/main/java/com/example/rudolph/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.rudolph;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_top_bar, menu);
+        return true;
     }
 }
