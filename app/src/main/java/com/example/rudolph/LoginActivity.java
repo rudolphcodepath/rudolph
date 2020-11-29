a6f55e653641d1594e804a05ea2f836ea969e359
Megan Ren <megan.j.ren.23@dartmouth.edu>
2020-11-28 20:24:07 -0500

    setting up login screen

---
+++ app/src/main/java/com/example/rudolph/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.example.rudolph;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+
+    public void onClick(View view) {
+    }
+}
