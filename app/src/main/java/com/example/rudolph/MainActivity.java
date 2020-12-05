3c5403f56ace9f8c375cf0c5cbc87dd20fc2140b
Megan Ren <megan.j.ren.23@dartmouth.edu>
2020-12-04 20:15:09 -0500

    wip

--- app/src/main/java/com/example/rudolph/MainActivity.java
+++ app/src/main/java/com/example/rudolph/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.rudolph;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 import com.firebase.ui.auth.AuthUI;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.Arrays;
 import java.util.List;
@@ -16,10 +21,35 @@ public class MainActivity extends AppCompatActivity {
 
     public static final int RC_SIGN_IN = 20;
 
+    final FragmentManager fragmentManager = getSupportFragmentManager();
+
+    BottomNavigationView bottomNavigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.menuNavbar);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment;
+                switch (item.getItemId()) {
+//                    case R.id.action_explore:
+//                        break;
+                    case R.id.action_profiles:
+                        fragment = new ProfilesFragment();
+                        break;
+                    default:
+                        fragment = new FindFragment();
+                }
+                fragmentManager.beginTransaction().replace(R.id.fLayout, fragment);
+                return true;
+            }
+        });
+
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
